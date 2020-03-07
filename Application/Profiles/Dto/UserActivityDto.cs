4ad4e16c64d954f0dd2561580dd2a281af04c9fc
yerkewka <yerkewka@mail.ru>
2020-03-08 00:08:26 +0600

    Server-Client: Added paging, sorting, filtering.

---
+++ Application/Profiles/Dto/UserActivityDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Application.Profiles.Dto
+{
+    public class UserActivitiesDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
